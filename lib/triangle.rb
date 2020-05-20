e844b6f9860a7397a5e3635b55945129b443652a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 23:14:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,8 +3,14 @@ class Triangle
 
   def initialize(side1, side2, side3)
     @side1 = side1
+    if @side1 == 0
+      return TriangleError
     @side2 = side2
+    if @side2 == 0
+      return TriangleError
     @side3 = side3
+    if @side3 == 0
+      return TriangleError
   end
 #equilateral = all sides
 #isosceles = 2 sides equal
