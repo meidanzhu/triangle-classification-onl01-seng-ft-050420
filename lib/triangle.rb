38395d92434570fdb2e6e2a3e07fbb16ac1f7f2d
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 23:07:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,18 +1,30 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_accessor :side1, :side2, :side3
 
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end
-
+#equilateral = all sides
+#isosceles = 2 sides equal
+#scalene = no sides equal
+# (side1 + side2) > side3 and sides > 0
   def kind
+    if (@side1 + @side2) >= @side3
+      return TriangleError
+    elsif @side1 == @side2 == @side 3
+      return :equilateral
+    end
+
+
+
+
 
   end
 
 
   class TriangleError < StandardError
   end
-  
+
 end
