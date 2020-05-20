a2c3d31157d74fe24a9d9b3d0a62b6ab5104cb8c
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 23:18:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,15 +3,15 @@ class Triangle
 
   def initialize(side1, side2, side3)
     @side1 = side1
-    if @side1 == 0
+    if @side1 <= 0
       return TriangleError
     end
     @side2 = side2
-    if @side2 == 0
+    if @side2 <= 0
       return TriangleError
     end
     @side3 = side3
-    if @side3 == 0
+    if @side3 <= 0
       return TriangleError
     end
   end
