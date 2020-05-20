8b733aabc0b9b932b2ffaadeba6028d9aff2ede1
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 22:52:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+
+  end
+
+
+  class TriangleError < StandardError
+  end
+  
 end
