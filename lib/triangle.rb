50b8641ad231095ee32e2f94e2ffe2a106bdd9f5
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 23:28:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
       raise TriangleError
     end
 
-    if (@side1 + @side2) <= @side3
+    if (@side1 + @side2) <= @side3 || (@side2 + @side3) <= @side1 || (@side1 + @side3) <= @side2
       raise TriangleError
     elsif (@side1 == @side2)&&(@side2 == @side3)&&(@side3 == @side1)
       return :equilateral
